0334a6ec0f132068c6ee5f17883bc778498b9a0b
Josh Lebedinsky <joshlebed@gmail.com>
2019-04-01 11:29:27 -0400

    made some changes to the geniusScraper.py, added pickleTransform.py

--- data/collectors/geniusScraper.py
+++ data/collectors/geniusScraper.py
@@ -6,82 +6,108 @@ import pickle
 
 base_url = 'http://api.genius.com'
 headers = {'Authorization': 'Bearer u0cAPtMZ8V-fhGqs2OGbEWyEe5HsrLb1BL1tMX-j6W6NYowtlq0_d5aWPUnvSYC0'}
+
+
 def docReader(file):
-    l = file.readlines()
-    return l
+  l = file.readlines()
+  return l
+
 
 def docConvert(q):
-    song_set = set([])
-    l = docReader(q)
-    for song in l:
-        s = song.split("|-+-|")
-        song_set.add((s[0],s[1].strip()))
-    return song_set
+  song_set = set([])
+  l = docReader(q)
+  for song in l:
+    s = song.split("|-+-|")
+    song_set.add((s[0],s[1].strip()))
+  return song_set
 
-def pickleing(lyric_dict, file):
-    pickle.dump(lyric_dict, open(file, 'wb'))
-
-# my_dict = {"2":{'title': "songtitle",'artist':"singer",}}
-
-# pickle(my_dict, 'file.pickle')
 
 def lyrics_from_song_api_path(song_api_path):
-    song_url = base_url + song_api_path
-    response = requests.get(song_url, headers=headers)
+  song_url = base_url + song_api_path
+  response = requests.get(song_url, headers=headers)
+  json = response.json()
+  path = json['response']['song']['path']
+
+  # Regular HTML scraping
+  page_url = 'http://genius.com' + path
+  page = requests.get(page_url)
+  html = BeautifulSoup(page.text, 'html.parser')
+  lyrics = html.find(class_='lyrics').get_text()
+  return lyrics
+
+
+def main(genre, path_in, path_out):
+  lyricData = {}
+  lyricData['genre'] = genre
+  lyricData['lyrics'] = {}
+
+  id = 0
+  number_tried = 0
+  number_found = 0
+
+  file_in = open(path_in, 'r')
+  content = docConvert(file_in)
+
+  print('found/attempted')
+
+  for item in content:
+    song_title = item[0]
+    artist_name = item[1]
+    search_url = base_url + '/search'
+    data = {'q': song_title}
+    response = requests.get(search_url, params=data, headers=headers)
+    song_info = None
     json = response.json()
-    path = json['response']['song']['path']
+    number_tried += 1
 
-    # Regular HTML scraping
-    page_url = 'http://genius.com' + path
-    page = requests.get(page_url)
-    html = BeautifulSoup(page.text, 'html.parser')
-    lyrics = html.find(class_='lyrics').get_text()
-    return lyrics
+    for hit in json['response']['hits']:
+      if hit['result']['primary_artist']['name'] == artist_name:
+        song_info = hit
+
+    if song_info:
+      number_found += 1
+      song_api_path = song_info['result']['api_path']
+      #print(song_info['result']['full_title'],": Found!")
+      my_string = (lyrics_from_song_api_path(song_api_path))
+      my_string = re.sub("\[.*?\]", "", my_string)
+      lyricData['lyrics'][id] = {"title":song_title,"artist":artist_name,"lyrics":my_string}
+      id+=1
+      print('id: {}'.format(id))
+
+      print('{}/{}'.format(number_found,number_tried))
+
+      # if(id%100 == 0):
+      #   with open('ChristianSongsLyrics.pickle', 'wb') as handle:
+      #     pickle.dump(ChristianSongs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+      #   with open('ChristianSongsLyrics.pickle', 'rb') as handle:
+      #     b = pickle.load(handle)
+
+    with open(path_out, 'wb') as pickle_file:
+      pickle.dump(lyricData, pickle_file)
+
+  print('done!')
+
+
 if __name__ == '__main__':
-    ChristianSongs = {}
-    id = 0
-    number_tried = 0
-    number_found = 0
-    assert len(sys.argv) > 1
-    filename = sys.argv[1]
-    input_file = open(filename,"r")
-    content = docConvert(input_file)
-    for item in content:
-        song_title = item[0]
-        artist_name = item[1]
-        search_url = base_url + '/search'
-        data = {'q': song_title}
-        response = requests.get(search_url, params=data, headers=headers)
-        song_info = None
-        json = response.json()
-        number_tried += 1
-        for hit in json['response']['hits']:
-            if hit['result']['primary_artist']['name'] == artist_name:
-                song_info = hit
-        if song_info:
-            number_found += 1
-            song_api_path = song_info['result']['api_path']
-            #print(song_info['result']['full_title'],": Found!")
-            my_string = (lyrics_from_song_api_path(song_api_path))
-            my_string = re.sub("\[.*?\]", "", my_string)
-            ChristianSongs[id] = {"title":song_title,"artist":artist_name,"lyrics":my_string}
-            id+=1
-            if(id%100 == 0):
-                with open('ChristianSongsLyrics.pickle', 'wb') as handle:
-                    pickle.dump(ChristianSongs, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                with open('ChristianSongsLyrics.pickle', 'rb') as handle:
-                    b = pickle.load(handle)
+  if len(sys.argv) < 4:
+    print('Usage: geniusScraper.py [genre name for object] [path to GenreSongs.txt] [path to fileout.pickle]')
+  else:
+    genre = sys.argv[1]
+    filein = sys.argv[2]
+    fileout = sys.argv[3]
+    main(genre, filein, fileout)
 
-            # if 'chorus' in my_string or 'Chorus' in my_string or 'CHORUS' in my_string:
-            #     print(my_string)
-        # else:
-        #     print(artist_name + ' - ' + song_title + ': Not found')
 
-        # print('current progress: {}/{}, {}%'
-        #     .format(number_found, number_tried, 100*number_found/number_tried))
+      # if 'chorus' in my_string or 'Chorus' in my_string or 'CHORUS' in my_string:
+      #   print(my_string)
+    # else:
+    #   print(artist_name + ' - ' + song_title + ': Not found')
+
+    # print('current progress: {}/{}, {}%'
+    #   .format(number_found, number_tried, 100*number_found/number_tried))
 
 
 
-#client id OtbFtWmkQc3Vf4R9oOKTRhSqrgtfvFOuODDhROhvJf3onFuiHe9hSHwmeAsSCO52
+# client id OtbFtWmkQc3Vf4R9oOKTRhSqrgtfvFOuODDhROhvJf3onFuiHe9hSHwmeAsSCO52
 # secret O8eDgg2tuyorMR0PB0S0GFyWoo7OMa1zFhWjfpTSCxE0Lz0ilIqFgerKaoiGKIKG_gJrNHAU_i01cAWPwF1LXg
-#access token u0cAPtMZ8V-fhGqs2OGbEWyEe5HsrLb1BL1tMX-j6W6NYowtlq0_d5aWPUnvSYC0
+# access token u0cAPtMZ8V-fhGqs2OGbEWyEe5HsrLb1BL1tMX-j6W6NYowtlq0_d5aWPUnvSYC0
---
+++ data/collectors/pickleTransform.py
@@ -0,0 +1,26 @@
+# open pickle and do some editing
+
+import sys
+import pickle
+
+def main(path_in, path_out):
+  with open(path_in, 'rb') as pickle_file:
+    lyricData = pickle.load(pickle_file)
+
+  print('genre is {}'.format(lyricData['genre']))
+
+  print(len(lyricData))
+  print(len(lyricData['lyrics']))
+
+  for songID in lyricData['lyrics']:
+    song = lyricData['lyrics'][songID]
+    print('songID: {}, artist: {}, first piece of lyrics: {}'.format(
+      songID, song['artist'], song['lyrics'][:100]))
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print('Usage: pickleTransform.py [path to filein.pickle] [path to fileout.pickle]')
+  else:
+    filein = sys.argv[1]
+    fileout = sys.argv[2]
+    main(filein, fileout)
