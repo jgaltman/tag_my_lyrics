b87d6d02649df723eae88fd441fb3a82b67ffa86
Josh Lebedinsky <joshlebed@gmail.com>
2019-04-01 10:16:23 -0400

    delete some files to clean up

--- data/collectors/query_spotipy.py
+++
@@ -1,7 +0,0 @@
-import spotipy
-username = 'joshlebed'
-util.prompt_for_user_token(username,scope,client_id='your-app-redirect-url',client_secret='your-app-redirect-url',redirect_uri='your-app-redirect-url')
-spotify = spotipy.Spotify()
-name = 'kendrick lamar'
-results = spotify.search(q='artist:' + name, type='artist')
-print(results)
